f9c47c11c875682df8b4aea4e446a69714683815
Todd Mpeli <toddowenmpeli@rocketmail.com>
2021-11-12 20:47:36 +0000

    Edited scss general styling and updated the index.js file content. Imported the bootstrap package.

--- pages/index.js
+++ pages/index.js
@@ -52,7 +52,14 @@ export default function Home() {
 								<a href="#store">Store</a>
 							</li>
 						</ul>
-						<div className={styles.navCheckout}></div>
+						<div className={styles.navCheckout}>
+							<a href="#searchProducts">
+								<i className="bi bi-search"></i>
+							</a>
+							<a href="#cart">
+								<i className="bi bi-bag-fill"></i>
+							</a>
+						</div>
 					</nav>
 				</header>
 				<h1 className={styles.title}>
@@ -63,36 +70,6 @@ export default function Home() {
 					Get started by editing{" "}
 					<code className={styles.code}>pages/index.js</code>
 				</p>
-
-				<div className={styles.grid}>
-					<a href="https://nextjs.org/docs" className={styles.card}>
-						<h2>Documentation &rarr;</h2>
-						<p>Find in-depth information about Next.js features and API.</p>
-					</a>
-
-					<a href="https://nextjs.org/learn" className={styles.card}>
-						<h2>Learn &rarr;</h2>
-						<p>Learn about Next.js in an interactive course with quizzes!</p>
-					</a>
-
-					<a
-						href="https://github.com/vercel/next.js/tree/master/examples"
-						className={styles.card}
-					>
-						<h2>Examples &rarr;</h2>
-						<p>Discover and deploy boilerplate example Next.js projects.</p>
-					</a>
-
-					<a
-						href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-						className={styles.card}
-					>
-						<h2>Deploy &rarr;</h2>
-						<p>
-							Instantly deploy your Next.js site to a public URL with Vercel.
-						</p>
-					</a>
-				</div>
 			</main>
 
 			<footer className={styles.footer}>
