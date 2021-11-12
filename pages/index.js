0a6cdc65b9a0db911e6557f8bf36fa02432974d5
Todd Mpeli <toddowenmpeli@rocketmail.com>
2021-11-12 23:59:03 +0000

    Updated the footer.

--- pages/index.js
+++ pages/index.js
@@ -97,22 +97,13 @@ export default function Home() {
 					/>
 
 					<h2 className={styles.title}>WEAR YOUR PASSION</h2>
-					<h5 className={styles.subtitle}>Anime Anime Anime</h5>
+					<h5 className={styles.subtitle}>
+						Define your style forward with Anime Anime Anime
+					</h5>
 				</div>
 			</main>
 
-			<footer className={styles.footer}>
-				<a
-					href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Powered by{" "}
-					<span className={styles.logo}>
-						<Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-					</span>
-				</a>
-			</footer>
+			<footer className={styles.footer}></footer>
 		</div>
 	);
 }
