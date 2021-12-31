e503c053aa4bbd00361daa75eb10ab6da086374b
Todd Mpeli <toddowenmpeli@rocketmail.com>
2021-12-31 21:03:45 +0000

    Created a termsConditions.js page.

---
+++ pages/AdditionalInfo/termsConditions.js
@@ -0,0 +1,13 @@
+import Head from "next/head";
+import Link from "next/link";
+import Image from "next/image";
+import {motion} from "framer-motion";
+import Navbar from "/components/Navbar";
+import Footer from "/components/Footer";
+import styles from "/styles/Home.module.scss";
+
+const termsConditions = () => {
+	return <div>Enter</div>;
+};
+
+export default termsConditions;
