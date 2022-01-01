5222cbc18a3aedab965a255644236aba9db7a7ae
Todd Mpeli <toddowenmpeli@rocketmail.com>
2022-01-01 15:34:43 +0000

    Created a onetime.js page and added Custom reusable Animation Properties/Objects.

---
+++ pages/OneTime.js
@@ -0,0 +1,44 @@
+import Head from "next/head";
+import Link from "next/link";
+import Image from "next/image";
+import {motion} from "framer-motion";
+import Navbar from "/components/Navbar";
+import Footer from "/components/Footer";
+import styles from "/styles/Home.module.scss";
+
+// Custom reusable Animation Properties/Objects
+const fadeInUp = {
+	initial: {
+		y: 60,
+		opacity: 0,
+	},
+	animate: {
+		y: 0,
+		opacity: 1,
+		transition: {duration: 0.5, ease: "easeOut"},
+	},
+};
+
+const fadeIn = {
+	initial: {
+		opacity: 0,
+	},
+	animate: {
+		opacity: 1,
+		transition: {duration: 0.5, delay: 1, ease: "easeOut"},
+	},
+};
+
+const stagger = {
+	animate: {
+		transition: {
+			staggerChildren: 0.1,
+		},
+	},
+};
+
+const OneTime = () => {
+	return <div>Enter</div>;
+};
+
+export default OneTime;
