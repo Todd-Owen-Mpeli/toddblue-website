a3a60ab86db1d31a1d376e1f8eb6611759c738f5
Todd Mpeli <toddowenmpeli@rocketmail.com>
2022-01-01 20:37:53 +0000

    Imorted framer-motion into the dark navigation and created Custom reusable Animation Properties/Objects.

--- components/alternativeStyling/NavbarDark.js
+++ components/alternativeStyling/NavbarDark.js
@@ -1,5 +1,37 @@
 import Link from "next/link";
+import {motion} from "framer-motion";
 import styles from "/styles/Home.module.scss";
+
+// Custom reusable Animation Properties/Objects
+const fadeInUp = {
+	initial: {
+		y: 60,
+		opacity: 0,
+	},
+	animate: {
+		y: 0,
+		opacity: 1,
+		transition: {duration: 0.5, ease: "easeOut"},
+	},
+};
+
+const fadeIn = {
+	initial: {
+		opacity: 0,
+	},
+	animate: {
+		opacity: 1,
+		transition: {duration: 0.5, delay: 1, ease: "easeOut"},
+	},
+};
+
+const stagger = {
+	animate: {
+		transition: {
+			staggerChildren: 0.1,
+		},
+	},
+};
 
 const NavbarDark = () => {
 	return (
