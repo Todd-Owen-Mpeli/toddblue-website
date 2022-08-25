15cf6aa72aa233bfa2bdadae3d08b98ac1ece23c
Todd Mpeli <toddowenmpeli@rocketmail.com>
2022-08-26 00:00:50 +0100

    Edited the missing import errors in vercel.

--- components/ProductCard.js
+++ components/ProductCard.js
@@ -7,51 +7,50 @@ const ProductCard = (props) => {
 	// console.log(props.image);
 
 	return (
-		<div>Hello</div>
-		// <motion.div
-		// 	initial={{
-		// 		y: 60,
-		// 		opacity: 0,
-		// 	}}
-		// 	animate={{
-		// 		y: 0,
-		// 		opacity: 1,
-		// 		transition: {
-		// 			duration: 0.5,
-		// 			ease: "easeOut",
-		// 		},
-		// 	}}
-		// 	className={styles.productItem}
-		// >
-		// 	<Link href="/store">
-		// 		<motion.a
-		// 			target="blank"
-		// 			whileHover={{scale: 1.0035}}
-		// 			whileTap={{scale: 1}}
-		// 		>
-		// 			<Image
-		// 				src={props.image}
-		// 				alt="Product Image"
-		// 				width={400}
-		// 				height={400}
-		// 				layout="responsive"
-		// 				objectFit="cover"
-		// 			/>
-		// 		</motion.a>
-		// 	</Link>
-		// 	<div className={styles.description}>
-		// 		<div className={styles.topSection}>
-		// 			<Link href="/store">
-		// 				<a target="blank">{props.productName}</a>
-		// 			</Link>
+		<motion.div
+			initial={{
+				y: 60,
+				opacity: 0,
+			}}
+			animate={{
+				y: 0,
+				opacity: 1,
+				transition: {
+					duration: 0.5,
+					ease: "easeOut",
+				},
+			}}
+			className={styles.productItem}
+		>
+			<Link href="/store">
+				<motion.a
+					target="blank"
+					whileHover={{scale: 1.0035}}
+					whileTap={{scale: 1}}
+				>
+					<Image
+						src={props.image}
+						alt="Product Image"
+						width={400}
+						height={400}
+						layout="responsive"
+						objectFit="cover"
+					/>
+				</motion.a>
+			</Link>
+			<div className={styles.description}>
+				<div className={styles.topSection}>
+					<Link href="/store">
+						<a target="blank">{props.productName}</a>
+					</Link>
 
-		// 			<h2 className={styles.price}>£{props.pricing}</h2>
-		// 		</div>
-		// 		<Link href="/store">
-		// 			<a className={styles.productCategory}>{props.productCategory}</a>
-		// 		</Link>
-		// 	</div>
-		// </motion.div>
+					<h2 className={styles.price}>£{props.pricing}</h2>
+				</div>
+				<Link href="/store">
+					<a className={styles.productCategory}>{props.productCategory}</a>
+				</Link>
+			</div>
+		</motion.div>
 	);
 };
 
