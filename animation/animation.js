5a7ad8cdfeee0aac938f655ca9a7f63a9d797705
Todd Mpeli <toddowenmpeli@rocketmail.com>
2022-08-26 00:14:24 +0100

    Edited .js files to be able to expost the array of objects and converted the animation.json to a .js file

--- animation/animation.js
+++ animation/animation.js
@@ -1,29 +1,31 @@
-[
+const animation = [
 	{
-		"animate": {
-			"transition": {
-				"staggerChildren": 0.1
-			}
-		}
+		animate: {
+			transition: {
+				staggerChildren: 0.1,
+			},
+		},
 	},
 	{
-		"initial": {
-			"opacity": 0
+		initial: {
+			opacity: 0,
 		},
-		"animate": {
-			"opacity": 1,
-			"transition": {"duration": 0.5, "delay": 1, "ease": "easeOut"}
-		}
+		animate: {
+			opacity: 1,
+			transition: {duration: 0.5, delay: 1, ease: "easeOut"},
+		},
 	},
 	{
-		"initial": {
-			"y": 60,
-			"opacity": 0
+		initial: {
+			y: 60,
+			opacity: 0,
 		},
-		"animate": {
-			"y": 0,
-			"opacity": 1,
-			"transition": {"duration": 0.5, "ease": "easeOut"}
-		}
-	}
-]
+		animate: {
+			y: 0,
+			opacity: 1,
+			transition: {duration: 0.5, ease: "easeOut"},
+		},
+	},
+];
+
+export default animation;
