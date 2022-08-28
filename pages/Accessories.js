2c76e3111aed4d20c1d62f3fd70fe45e9cbb8862
Todd Mpeli <toddowenmpeli@rocketmail.com>
2022-08-28 22:34:48 +0100

    Fixed the crossBagProducts.js file. import in the accesories page.

--- pages/Accessories.js
+++ pages/Accessories.js
@@ -2,11 +2,11 @@ import Head from "next/head";
 import {motion} from "framer-motion";
 import styles from "/styles/Home.module.scss";
 import accessories from "/lib/accessories.js";
-import crossBagProducts from "/lib/crossBagProducts.js";
+// import crossBagProducts from "/lib/crossBagProducts.js";
 import ProductDisplayFourGrid from "/components/productDisplayFourGrid";
 
 const Accessories = () => {
-	const allAccessories = [...crossBagProducts, ...accessories];
+	const allAccessories = [...accessories];
 
 	return (
 		<motion.div
